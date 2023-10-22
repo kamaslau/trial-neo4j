4dbcedfc5f95dfe3925c7249af456a0a10ca83e5
Kamas 'Iceberg' Lau <kamaslau@users.noreply.github.com>
2023-10-22 16:10:54 +0800

    chore: docker compose testimony

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # trial-neo4j
 
+[Getting started with Neo4j in Docker](https://neo4j.com/docs/operations-manual/current/docker/introduction/)
 [Neo4j JavaScript Driver](https://neo4j.com/docs/javascript-manual/current/)
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-import Koa from "koa";
+// https://neo4j.com/docs/javascript-manual/current/
+import * as neo4j from "neo4j-driver";
 
-const app = new Koa();
+const url = "neo4j://localhost";
+const dbUser = "neo4j";
+const dbPwd = "12345678";
 
-// TODO Import and demo Neo4j official Node.js driver
+(async () => {
+  let driver;
 
-app.use(async (ctx) => {
-  ctx.body = "Hello World!";
-});
-
-app.listen(3000);
+  try {
+    driver = neo4j.driver(url, neo4j.auth.basic(dbUser, dbPwd));
+    const serverInfo = await driver.getServerInfo();
+    console.log("Connection established");
+    console.log(serverInfo);
+  } catch (err) {
+    console.log(`Connection error\n${err}\nCause: ${err.cause}`);
+  }
+})();
